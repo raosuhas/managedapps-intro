c908fe2a1b1a46e3392a4d17438654b1d382d457
Himanshu Chandel <hich@microsoft.com>
2019-05-01 17:08:59 -0700

    Function with code and zip

---
+++ SampleFunction/SharePointManagementRPWithFunction/SharePointManagementFunc/SharePointManagementFunc/Models/CreateEventModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePointManager.Models
+{
+    internal class CreateEventModel
+    {
+        public string Title { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+    }
+}
